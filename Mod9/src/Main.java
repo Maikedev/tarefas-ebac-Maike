97bbfe6ed01c45a63370ca10124c0ddd360256ab
Maikedev <136399095+Maikedev@users.noreply.github.com>
2025-02-22 15:39:43 -0300

    Add files via upload

---
+++ Mod9/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int num = 34;
+        System.out.println(num);
+        short num2 = (short) num;
+        System.out.println(num2);
+    }
+}
