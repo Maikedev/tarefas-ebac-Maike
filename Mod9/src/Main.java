be73fab91aa947eb6dd0866b17f7f5ee7f9db393
Maikedev <136399095+Maikedev@users.noreply.github.com>
2025-02-22 16:04:18 -0300

    Update Main.java

--- Mod9/src/Main.java
+++ Mod9/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int num = 34;
         System.out.println(num);
-        short num2 = (short) num;
+        Integer num2 = Integer.valueOf(num);
         System.out.println(num2);
     }
 }
